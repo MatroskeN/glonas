9584806228014fa83f6c215793345cafe4e82e61
Vlad <44238649+MatroskeN@users.noreply.github.com>
2022-03-20 01:04:53 +0300

    adaptive + popup

---
+++ script/main.js
@@ -0,0 +1,7 @@
+document.querySelector('.getSolution').onclick = function (){
+    document.querySelector('.mask').style.display = "flex";
+}
+
+document.querySelector('.xclose').onclick = function (){
+    document.querySelector('.mask').style.display = "none";
+}
