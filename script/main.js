950381a3d3fdf07d776ebfb9dc2364f6abad01f8
Vlad <44238649+MatroskeN@users.noreply.github.com>
2022-03-20 09:34:10 +0300

    cars

--- script/main.js
+++ script/main.js
@@ -5,3 +5,30 @@ document.querySelector('.getSolution').onclick = function (){
 document.querySelector('.xclose').onclick = function (){
     document.querySelector('.mask').style.display = "none";
 }
+$( document ).ready(function() {
+    $(".cargo").click(function (){
+        $(".switch").removeClass('active');
+        $(this).addClass('active');
+        $(".truckPic").attr('src','images/truck.png');
+    });
+    $(".taxi").click(function (){
+        $(".switch").removeClass('active');
+        $(this).addClass('active');
+        $(".truckPic").attr('src','images/taxi.png');
+    });
+    $(".corp").click(function (){
+        $(".switch").removeClass('active');
+        $(this).addClass('active');
+        $(".truckPic").attr('src','images/corp.png');
+    });
+    $(".spec").click(function (){
+        $(".switch").removeClass('active');
+        $(this).addClass('active');
+        $(".truckPic").attr('src','images/spec.png');
+    });
+    $(".passenger").click(function (){
+        $(".switch").removeClass('active');
+        $(this).addClass('active');
+        $(".truckPic").attr('src','images/passenger.png');
+    });
+});
