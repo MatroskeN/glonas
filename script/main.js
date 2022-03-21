b080dbf1d06960a52398d384ca995657ea16f8b2
Vlad <44238649+MatroskeN@users.noreply.github.com>
2022-03-21 15:45:57 +0300

    fix

--- script/main.js
+++ script/main.js
@@ -10,25 +10,35 @@ $( document ).ready(function() {
         $(".switch").removeClass('active');
         $(this).addClass('active');
         $(".truckPic").attr('src','images/truck.png');
+        $(".equipmentSubtitle").html("<div>Абонентский терминал ГЛОНАСС/GPS;<br/>Датчики топлива.</div>");
+        $(".controlSubtitle").html("<div>Местоположения, скорости, моточасов, посещения контрольных точек, соблюдения маршрута и расхода топлива в режиме реального времени.</div>");
     });
     $(".taxi").click(function (){
         $(".switch").removeClass('active');
         $(this).addClass('active');
         $(".truckPic").attr('src','images/taxi.png');
+        $(".equipmentSubtitle").html("<div>Абонентский терминал ГЛОНАСС/GPS</div>");
+        $(".controlSubtitle").html("<div>работы и простоя ТС, маршрутов, пробега, стиля вождения, скоростного режима, нахождения в пределах заданной геозоны, сроков ТО</div>");
     });
     $(".corp").click(function (){
         $(".switch").removeClass('active');
         $(this).addClass('active');
         $(".truckPic").attr('src','images/corp.png');
+        $(".equipmentSubtitle").html("<div>Абонентский терминал ГЛОНАСС/GPS</div>");
+        $(".controlSubtitle").html("<div>работы и простоя ТС, маршрутов, пробега, стиля вождения, скоростного режима, нахождения в пределах заданной геозоны, сроков ТО</div>");
     });
     $(".spec").click(function (){
         $(".switch").removeClass('active');
         $(this).addClass('active');
         $(".truckPic").attr('src','images/spec.png');
+        $(".equipmentSubtitle").html("<div>Абонентский терминал ГЛОНАСС/GPS</div>");
+        $(".controlSubtitle").html("<div>работы и простоя ТС, маршрутов, пробега, стиля вождения, скоростного режима, нахождения в пределах заданной геозоны, сроков ТО</div>");
     });
     $(".passenger").click(function (){
         $(".switch").removeClass('active');
         $(this).addClass('active');
         $(".truckPic").attr('src','images/passenger.png');
+        $(".equipmentSubtitle").html("<div>Абонентский терминал ГЛОНАСС/GPS</div>");
+        $(".controlSubtitle").html("<div>работы и простоя ТС, маршрутов, пробега, стиля вождения, скоростного режима, нахождения в пределах заданной геозоны, сроков ТО</div>");
     });
 });
