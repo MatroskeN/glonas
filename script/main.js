15e8027e58e953756d55d9fd0aace390215b0b15
Vlad <44238649+MatroskeN@users.noreply.github.com>
2022-03-24 15:20:01 +0300

    patch

--- script/main.js
+++ script/main.js
@@ -1,3 +1,4 @@
+//open modal function
 document.querySelector('.getSolution').onclick = function (){
     document.querySelector('.mask').style.display = "flex";
 }
@@ -5,7 +6,9 @@ document.querySelector('.getSolution').onclick = function (){
 document.querySelector('.xclose').onclick = function (){
     document.querySelector('.mask').style.display = "none";
 }
+
 $( document ).ready(function() {
+    //desktop tabs change function
     $(".cargo").click(function (){
         $(".switch").removeClass('active');
         $(this).addClass('active');
@@ -41,4 +44,31 @@ $( document ).ready(function() {
         $(".equipmentSubtitle").html("<div>Абонентский терминал ГЛОНАСС/GPS</div>");
         $(".controlSubtitle").html("<div>работы и простоя ТС, маршрутов, пробега, стиля вождения, скоростного режима, нахождения в пределах заданной геозоны, сроков ТО</div>");
     });
+//mobTabs change function
+    $(".selectCar").change(function (){
+        switch ($(this).val()){
+            case 'truck':
+                $(".equipmentSubtitle").html("<div>Абонентский терминал ГЛОНАСС/GPS;<br/>Датчики топлива.</div>");
+                $(".controlSubtitle").html("<div>Местоположения, скорости, моточасов, посещения контрольных точек, соблюдения маршрута и расхода топлива в режиме реального времени.</div>");
+            break;
+            case 'taxi':
+                $(".equipmentSubtitle").html("<div>Абонентский терминал ГЛОНАСС/GPS</div>");
+                $(".controlSubtitle").html("<div>работы и простоя ТС, маршрутов, пробега, стиля вождения, скоростного режима, нахождения в пределах заданной геозоны, сроков ТО</div>");
+            break;
+            case 'corp':
+                $(".equipmentSubtitle").html("<div>Абонентский терминал ГЛОНАСС/GPS</div>");
+        $(".controlSubtitle").html("<div>работы и простоя ТС, маршрутов, пробега, стиля вождения, скоростного режима, нахождения в пределах заданной геозоны, сроков ТО</div>");
+            break;
+            case 'spec':
+                $(".equipmentSubtitle").html("<div>Абонентский терминал ГЛОНАСС/GPS</div>");
+        $(".controlSubtitle").html("<div>работы и простоя ТС, маршрутов, пробега, стиля вождения, скоростного режима, нахождения в пределах заданной геозоны, сроков ТО</div>");
+            break;
+            case 'passenger':
+                $(".equipmentSubtitle").html("<div>Абонентский терминал ГЛОНАСС/GPS</div>");
+        $(".controlSubtitle").html("<div>работы и простоя ТС, маршрутов, пробега, стиля вождения, скоростного режима, нахождения в пределах заданной геозоны, сроков ТО</div>");
+            break;
+            default:
+                console.log('Oops');
+        }
+    })
 });
